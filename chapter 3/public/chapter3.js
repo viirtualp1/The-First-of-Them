925911166e157c1a11e299159ca695b6d41980ca
Nikita <zininnikita309@gmail.com>
2020-09-12 22:20:39 +0900

    Source code

--- chapter 3/public/chapter3.js
+++ chapter 3/public/chapter3.js
@@ -33,7 +33,7 @@ function hello_team() {
     btn_next = document.getElementById("btn-next");
     btn_next = btn_next.parentNode.removeChild(btn_next);
     document.getElementById("footer-buttons").innerHTML += `
-        <button id="btn-next" type="button" class="btn btn-lg btn-block btn-dark" onclick="training()">Далее</button>
+        <button id="btn-next" type="button" class="btn btn-lg btn-dark" onclick="training()">Далее</button>
     `;
 }
 
@@ -96,8 +96,110 @@ function go_to_mountains() {
     btn_next = document.getElementById("btn-next");
     btn_next = btn_next.parentNode.removeChild(btn_next);
     document.getElementById("footer-buttons").innerHTML += `
-        <button id="btn-next" type="button" class="btn btn-lg btn-block btn-dark" onclick="bloger_theme()">Далее</button>
+        <button id="btn-next" type="button" class="btn btn-lg btn-dark" onclick="change_hero('Б', 'Л', 'О', 'Г', 'Е', 'Р', '#f33b3b')">Далее</button>
     `;
 }
 
-function bloger_theme() { location.href = "bloger-theme.html"; }
+function change_hero(letter1, letter2, letter3,
+                        letter4, letter5, letter6, 
+                            shadowColor) {
+    document.body.style.background = "#000";
+    music.src = "";
+    sound.src = "sounds/Royalty Free - Digital Number FX.mp3";
+
+    document.getElementById("name").style.visibility = "hidden";
+    document.getElementById("footer-buttons").style.visibility = "hidden";
+    
+    dialogs_div.innerHTML = `
+        <div class="container d-flex justify-content-center" id="name-text-div">
+            <div>
+                <h1 style="float: left;" class="title-show1" id="letter-1">${letter1}</h1>
+                <h1 style="float: left;" class="title-show2" id="letter-2">${letter2}</h1>
+                <h1 style="float: left;" class="title-show3" id="letter-3">${letter3}</h1>
+                <h1 style="float: left;" class="title-show4" id="letter-4">${letter4}</h1>
+                <h1 style="float: left;" class="title-show5" id="letter-5">${letter5}</h1>
+                <h1 style="float: left;" class="title-show6" id="letter-6">${letter6}</h1>
+            </div>
+        </div>
+    `;
+
+    // Анимация появления имени
+    setTimeout(() => {
+        let p1 = document.getElementById("letter-1");
+        let p2 = document.getElementById("letter-2");
+        let p3 = document.getElementById("letter-3");
+        let p4 = document.getElementById("letter-4");
+        let p5 = document.getElementById("letter-5");
+        let p6 = document.getElementById("letter-6");
+
+        // Анимация появления имени
+
+        p1.style.textShadow = `1px 1px 10px ${shadowColor}`
+        p1.id = "title-show1";
+
+        p2.style.textShadow = `1px 1px 10px ${shadowColor}`
+        p2.id = "title-show2";
+
+        p3.style.textShadow = `1px 1px 10px ${shadowColor}`
+        p3.id = "title-show3";
+
+        p4.style.textShadow = `1px 1px 10px ${shadowColor}`
+        p4.id = "title-show4";
+
+        p5.style.textShadow = `1px 1px 10px ${shadowColor}`
+        p5.id = "title-show5";
+
+        p6.style.textShadow = `1px 1px 10px ${shadowColor}`
+        p6.id = "title-show6";
+    }, 200)
+
+    setTimeout(() => {
+        bloger_house();
+    }, 6000);
+}
+
+function bloger_house() {
+    music.src = "../../The First of Them (Chapter 1+Chapter 2)/chapter1/music/night_jungle.mp3";
+    document.getElementById("name").style.visibility = "visible";
+    document.getElementById("footer-buttons").style.visibility = "visible";
+
+    document.body.style.background = "url('img/house.jpg')"
+
+    let p1 = document.getElementById("title-show1");
+    let p2 = document.getElementById("title-show2");
+    let p3 = document.getElementById("title-show3");
+    let p4 = document.getElementById("title-show4");
+    let p5 = document.getElementById("title-show5");
+    let p6 = document.getElementById("title-show6");
+
+    p1.parentNode.removeChild(p1);
+    p2.parentNode.removeChild(p2);
+    p3.parentNode.removeChild(p3);
+    p4.parentNode.removeChild(p4);
+    p5.parentNode.removeChild(p5);
+    p6.parentNode.removeChild(p6);
+
+    name_dialog.innerHTML = "Блогер";
+    dialogs_div.innerHTML = `
+        <div id="dialogs">
+            <div class="row dialog">
+                <p class="lead" id="text-dialog">Так, вот разрушенный особняк, 
+                    нужно посмотреть осталось ли что здесь,
+                    по моим предположениям, лаборатория должна быть где-то внизу
+                </p>
+            </div>
+        </div>
+    `;
+
+    btn_next = document.getElementById("btn-next");
+    btn_next = btn_next.parentNode.removeChild(btn_next);
+    document.getElementById("footer-buttons").innerHTML += `
+        <button id="btn-next" type="button" class="btn btn-lg btn-dark" onclick="house()">Далее</button>
+    `;
+}
+
+{/* <div id="dialogs">
+<div class="row dialog">
+    <p class="lead" id="text-dialog">Так, вот дверь, но нужно расчистить путь</p>
+</div>
+</div> */}
