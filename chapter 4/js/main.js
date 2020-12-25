2735328b6bc080f6ac83981c128ed0035f77afc7
Nikita <zininnikita309@gmail.com>
2020-12-25 21:17:59 +0900

    Source code

--- chapter 4/js/main.js
+++ chapter 4/js/main.js
@@ -48,6 +48,8 @@
 //     alert('Fake key');
 // }
 
+// (DEG) NIKITA // 
+
 function vasiliyStart() {
     // changeBg('jungle-house');
 
@@ -93,12 +95,138 @@ function houseInside() {
 }
 
 function rainSleepVasiliy() {
+    setTimeout(() => {
+        // changeBg('rainSleepVasiliy');
+
+        sound.src = 'sounds/sleepBullet.mp3';
+
+        footerButtons.style.visibility = 'hidden';
+
+        dialogs.innerHTML = `
+            <div class="row dialog">
+                <p class="lead" id="text-dialog">Ааа... Что..</p>
+            </div>
+        `;
+
+        dialogsDiv.style.transition = '7s ease';
+        dialogsDiv.style.opacity = 0;
+
+        nameDiv.style.transition = '7s ease';
+        nameDiv.style.opacity = 0;
+    }, 1000);
+
+    setTimeout(() => {
+        document.body.style.background = '#000';
+
+        setTimeout(() => {
+            changeHeroPolina();
+        }, 2500);
+    }, 7500);
+}
+
+function changeHeroPolina() {
+    document.body.style.background = '#000';
+    music.src = '';
+    sound.src = 'sounds/polina_change.mp3';
+
+    dialogsDiv.style.opacity = 1;
+    document.getElementById('name').style.visibility = 'hidden';
+    document.getElementById('footer-buttons').style.visibility = 'hidden';
+
     dialogs.innerHTML = `
-        <div class="row dialog">
-            <p class="lead" id="text-dialog"></p>
+        <div class="container d-flex justify-content-center" id="name-text-div">
+            <div>
+                <h1 style="float: left;" class="title-show1" id="letter-1">П</h1>
+                <h1 style="float: left;" class="title-show2" id="letter-2">о</h1>
+                <h1 style="float: left;" class="title-show3" id="letter-3">л</h1>
+                <h1 style="float: left;" class="title-show4" id="letter-4">и</h1>
+                <h1 style="float: left;" class="title-show5" id="letter-5">н</h1>
+                <h1 style="float: left;" class="title-show6" id="letter-6">а</h1>
+            </div>
         </div>
     `;
 
-    btnNext();
+    // Анимация появления имени
+    setTimeout(() => {
+        const p1 = document.getElementById('letter-1');
+        const p2 = document.getElementById('letter-2');
+        const p3 = document.getElementById('letter-3');
+        const p4 = document.getElementById('letter-4');
+        const p5 = document.getElementById('letter-5');
+        const p6 = document.getElementById('letter-6');
+
+        // Анимация появления имени
+        p1.style.textShadow = `1px 1px 10px rgb(198, 85, 250)`;
+        p1.id = 'title-show1';
+
+        p2.style.textShadow = `1px 1px 10px rgb(198, 85, 250)`;
+        p2.id = 'title-show2';
+
+        p3.style.textShadow = `1px 1px 10px rgb(198, 85, 250)`;
+        p3.id = 'title-show3';
+
+        p4.style.textShadow = `1px 1px 10px rgb(198, 85, 250)`;
+        p4.id = 'title-show4';
+
+        p5.style.textShadow = `1px 1px 10px rgb(198, 85, 250)`;
+        p5.id = 'title-show5';
+
+        p6.style.textShadow = `1px 1px 10px rgb(198, 85, 250)`;
+        p6.id = 'title-show6';
+    }, 200);
+
+    setTimeout(() => {
+        try {
+            const p1 = document.getElementById('title-show1');
+            const p2 = document.getElementById('title-show2');
+            const p3 = document.getElementById('title-show3');
+            const p4 = document.getElementById('title-show4');
+            const p5 = document.getElementById('title-show5');
+            const p6 = document.getElementById('title-show6');
+
+            document.body.style.background = `url('img/new-house.jpg')`;
+            document.body.style.backgroundPosition = 'center';
+            document.body.style.backgroundRepeat = 'no-repeat';
+            document.body.style.backgroundAttachment = 'fixed';
+            document.body.style.backgroundSize = 'cover';
+
+            p1.parentNode.removeChild(p1);
+            p2.parentNode.removeChild(p2);
+            p3.parentNode.removeChild(p3);
+            p4.parentNode.removeChild(p4);
+            p5.parentNode.removeChild(p5);
+            p6.parentNode.removeChild(p6);
+
+            nameDiv.style.opacity = 1;
+            nameDiv.style.transition = 0;
+            footerButtons.style.visibility = 'visible';
+        } catch { }
+
+        houseWakeUp();
+    }, 5000);
 }
 
+function houseWakeUp() {
+    // changeBg('houseNew');
+
+}
+
+// WARNING //
+// FUTURE CODE //
+// function futureFunction() {
+//     localStorage.setItem('currentFunction', 'future(Current)Function');
+// }
+
+// window.onload = () => {
+//     const currentFunctionLc = localStorage.getItem('currentFunction');
+
+//     if (currentFunctionLc != null) {
+//         if (currentFunctionLc == 'future(Current)Function') {
+//             futureFunction();
+//         }
+//     } else { }
+// };
+
+function transition() {
+    window.location.assign("././searchItems/index-nosplash.html");
+}
